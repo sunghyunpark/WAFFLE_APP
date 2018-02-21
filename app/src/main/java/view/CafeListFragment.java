1677406d7842bf5883b2bea42fef7ffff37a2e7c
sunghyun <sunghyun1038@naver.com>
2018-02-21 13:54:05 +0900

    IntroActivity add

--- app/src/main/java/view/CafeListFragment.java
+++ app/src/main/java/view/CafeListFragment.java
@@ -1,18 +1,23 @@
 package view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.cafemobile.waffle.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class CafeListFragment extends Fragment {
 
-    public CafeListFragment() {
-        // Required empty public constructor
-    }
+    View v;
+    @BindView(R.id.go_intro_btn) Button goIntroBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,17 @@ public class CafeListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cafe_list, container, false);
+        v = inflater.inflate(R.layout.fragment_cafe_list, container, false);
+        ButterKnife.bind(this, v);
+
+        return v;
     }
+
+    @OnClick(R.id.go_intro_btn) void goIntro(){
+        Intent intent = new Intent(getContext(), IntroActivity.class);
+        startActivity(intent);
+    }
+
+
 
 }
