cfd6dcdd0d65c316e69eb69901f8a6ac7914ef0d
sunghyun <sunghyun1038@naver.com>
2018-02-21 13:55:55 +0900

    IntroActivity add

---
+++ app/src/main/java/com/cafemobile/waffle/WaffleApplication.java
@@ -0,0 +1,48 @@
+package com.cafemobile.waffle;
+
+import android.app.Application;
+import android.content.res.Configuration;
+import android.view.Display;
+import android.view.WindowManager;
+
+/**
+ * Created by SungHyun on 2018-02-21.
+ */
+
+public class WaffleApplication extends Application{
+    public static final String SERVER_BASE_IP = "http://13.124.188.3/chapter/";
+    public static int DISPLAY_WIDTH;
+    public static int DISPLAY_HEIGHT;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Display display;
+        display = ((WindowManager)getApplicationContext().getSystemService(getApplicationContext().WINDOW_SERVICE)).getDefaultDisplay();
+        DISPLAY_HEIGHT = display.getHeight();
+        DISPLAY_WIDTH = display.getWidth();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    public int getDISPLAY_WIDTH() {
+        return DISPLAY_WIDTH;
+    }
+
+    public int getDISPLAY_HEIGHT() {
+        return DISPLAY_HEIGHT;
+    }
+}
---
+++ app/src/main/java/view/IntroActivity.java
@@ -0,0 +1,15 @@
+package view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cafemobile.waffle.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
