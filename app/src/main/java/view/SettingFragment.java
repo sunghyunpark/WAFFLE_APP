952e384f0838f66d0a45ef686bde5228fd90ede8
sunghyunpark <sunghyun1038@naver.com>
2018-03-09 22:10:04 +0900

    SettingFragment 적용중

--- app/src/main/java/view/SettingFragment.java
+++ app/src/main/java/view/SettingFragment.java
@@ -1,25 +1,44 @@
 package view;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.cafemobile.waffle.R;
 
+import butterknife.BindString;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import model.UserModel;
 import presenter.LoginPresenter;
 
 public class SettingFragment extends Fragment {
 
     View v;
+    @BindView(R.id.user_name_txt) TextView user_name_tv;
+    @BindView(R.id.user_email_txt) TextView user_email_tv;
+    @BindView(R.id.user_profile_edit_layout) ViewGroup userProfileEditLayout;
+    @BindView(R.id.app_version_txt) TextView app_version_tv;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        SetUI();
     }
 
     @Override
@@ -30,6 +49,20 @@ public class SettingFragment extends Fragment {
         ButterKnife.bind(this, v);
 
         return v;
+    }
+
+    private void SetUI(){
+        user_name_tv.setText(UserModel.getInstance().getName());
+        user_email_tv.setText(UserModel.getInstance().getEmail());
+
+        //앱 버전
+        String version;
+        try {
+            PackageInfo i = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+            version = i.versionName;
+            app_version_tv.setText("v"+version);
+        } catch(PackageManager.NameNotFoundException e) { }
+
     }
 
     @OnClick(R.id.logout_btn) void logoutClicked(){
